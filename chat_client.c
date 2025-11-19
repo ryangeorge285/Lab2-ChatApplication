6929ad6549e6f07e15d348c0814dbfbd936f3bef
Rishabh <rishabh.rstgi06@gmail.com>
2025-11-19 11:58:12 +0000

    Added skeleton code for listener and sender threads

--- chat_client.c
+++ chat_client.c
@@ -2,56 +2,46 @@
 #include <stdio.h>
 #include "udp.h"
 #include "chat_parser.h"
+#include <pthread.h>
 
 #define CLIENT_PORT 10001
+
+int sd;
+struct sockaddr_in server_addr;
+
+
+void *sender_thread(void *arg);
+void *listener_thread(void *arg);
+
 
 // client code
 int main(int argc, char *argv[])
 {
-    // This function opens a UDP socket,
-    // binding it to all IP interfaces of this machine,
-    // and port number CLIENT_PORT.
-    // (See details of the function in udp.h)
-    int sd = udp_socket_open(CLIENT_PORT);
+    sd = udp_socket_open(CLIENT_PORT);
 
-    // Variable to store the server's IP address and port
-    // (i.e. the server we are trying to contact).
-    // Generally, it is possible for the responder to be
-    // different from the server requested.
-    // Although, in our case the responder will
-    // always be the same as the server.
-    struct sockaddr_in server_addr, responder_addr;
 
     // Initializing the server's address.
     // We are currently running the server on localhost (127.0.0.1).
     // You can change this to a different IP address
     // when running the server on a different machine.
     // (See details of the function in udp.h)
+
+
     int rc = set_socket_addr(&server_addr, "127.0.0.1", SERVER_PORT);
 
     // Storage for request and response messages
     char client_request[BUFFER_SIZE], server_response[BUFFER_SIZE];
 
-    // Demo code (remove later)
-    strcpy(client_request, "Dummy Request");
+    // Creating two threads:
+    pthread_t sender_tid, listener_tid;
+    pthread_create(&sender_tid, NULL, sender_thread, NULL);
+    pthread_create(&listener_tid, NULL, listener_thread, NULL);
 
-    // This function writes to the server (sends request)
-    // through the socket at sd.
-    // (See details of the function in udp.h)
-    rc = udp_socket_write(sd, &server_addr, client_request, BUFFER_SIZE);
 
-    if (rc > 0)
-    {
-        // This function reads the response from the server
-        // through the socket at sd.
-        // In our case, responder_addr will simply be
-        // the same as server_addr.
-        // (See details of the function in udp.h)
-        int rc = udp_socket_read(sd, &responder_addr, server_response, BUFFER_SIZE);
+    pthread_join(sender_tid, NULL);
+    pthread_join(listener_tid, NULL);
 
-        // Demo code (remove later)
-        printf("server_response: %s", server_response);
-    }
+    
 
     return 0;
 }
